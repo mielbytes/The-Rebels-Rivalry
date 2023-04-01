0394c81575ad71abaffefc6223fb189845d0fa2e
Deadzone <104700583+DeadzoneRL@users.noreply.github.com>
2023-04-01 09:23:47 -0700

    Add files via upload

---
+++ Global_Variables.lua
@@ -0,0 +1,5 @@
+_G.enemies = {}
+_G.colliders = {}
+_G.bullets = {}
+_G.window_width = 1280
+_G.window_height = 720 -- these variables will change in an update function --> window_width, window_height = love.window.getMode()
