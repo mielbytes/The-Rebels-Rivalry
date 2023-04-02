21a7fdb0d4d5d4bbbc3eb936c6a9530c20c9c6a7
mielbytes <129541995+mielbytes@users.noreply.github.com>
2023-04-02 02:54:05 +0200

    Delete conf.lua

--- conf.lua
+++
@@ -1,8 +0,0 @@
-function love.conf(t)
-  t.title = "The Rebel's Rivalry"
-  t.window.width = 1280
-  t.window.height = 720
-  t.window.resizable = true
-  t.window.fullscreen = true
-  t.console = true
-end
